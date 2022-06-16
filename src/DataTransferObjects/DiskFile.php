6e195f9ac3f12d7a17932291c25f2d63ffb6df0b
Andre Inocenti <andreinocenti@gmail.com>
2022-06-16 20:43:00 -0300

    feat: DiskFile dto

---
+++ src/DataTransferObjects/DiskFile.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace AndreInocenti\LaravelFileS3Like\DataTransferObjects;
+
+class DiskFile
+{
+    public function __construct(
+        protected string $filepath,
+        protected string $filename,
+        protected string $url,
+        protected ?string $extension = null
+    ) {
+    }
+
+    /**
+     * Return the filepath.
+     *
+     * @return string
+     */
+    public function getFilepath(): string
+    {
+        return $this->filepath;
+    }
+
+    /**
+     * Returns the filename
+     *
+     * @return string
+     */
+    public function getFilename(): string
+    {
+        return $this->filename;
+    }
+
+    /**
+     * returns the full file url
+     *
+     * @return string
+     */
+    public function getUrl(): string
+    {
+        return $this->url;
+    }
+
+    /**
+     * returns the file extension
+     *
+     * @return string
+     */
+    public function getExtension(): string
+    {
+        return $this->extension;
+    }
+}
