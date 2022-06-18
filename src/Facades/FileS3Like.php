49e76b73c92c68a681b9a0a56cad4ce407632a3f
Andre Inocenti <andreinocenti@gmail.com>
2022-06-18 11:10:43 -0300

    fix: FileS3Like facade class

--- src/Facades/FileS3Like.php
+++ src/Facades/FileS3Like.php
@@ -1,6 +1,7 @@
 <?php
 namespace AndreInocenti\LaravelFileS3Like\Facades;
 
+use AndreInocenti\LaravelFileS3Like\FileS3Like as LaravelFileS3LikeFileS3Like;
 use Illuminate\Support\Facades\Facade;
 
 
@@ -8,6 +9,6 @@ class FileS3Like extends Facade
 {
     protected static function getFacadeAccessor()
     {
-        return 'file-s3-like';
+        return LaravelFileS3LikeFileS3Like::class;
     }
 }
