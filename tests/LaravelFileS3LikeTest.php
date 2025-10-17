6b4f3e5ae17c47b48c662f2b196c6065bbad94f0
Andre Inocenti <andreinocenti@gmail.com>
2025-10-17 19:08:34 -0300

    feat: tests env

--- tests/LaravelFileS3LikeTest.php
+++ tests/LaravelFileS3LikeTest.php
@@ -19,19 +19,19 @@ class LaravelFileS3LikeTest extends \Orchestra\Testbench\TestCase
     protected function getEnvironmentSetUp($app)
     {
         // perform environment setup
-        // $app['config']->set("filesystems.disks.spaces", [
-        //     'driver' => 's3',
-        //     'key' => env('SPACES_ACCESS_KEY_ID'),
-        //     'secret' => env('SPACES_SECRET_ACCESS_KEY'),
-        //     'region' => env('SPACES_DEFAULT_REGION'),
-        //     'bucket' => env('SPACES_BUCKET'),
-        //     'url' => env('SPACES_URL'),
-        //     'endpoint' => env('SPACES_ENDPOINT'),
-        //     'folder' => env('SPACES_FOLDER'),
-        //     'cdn_endpoint' => env('SPACES_CDN_ENDPOINT'),
-        //     'use_path_style_endpoint' => env('SPACES_USE_PATH_STYLE_ENDPOINT', false),
-        //     'throw' => false,
-        // ]);
+        $app['config']->set("filesystems.disks.spaces", [
+            'driver' => 's3',
+            'key' => env('SPACES_ACCESS_KEY_ID'),
+            'secret' => env('SPACES_SECRET_ACCESS_KEY'),
+            'region' => env('SPACES_DEFAULT_REGION'),
+            'bucket' => env('SPACES_BUCKET'),
+            'url' => env('SPACES_URL'),
+            'endpoint' => env('SPACES_ENDPOINT'),
+            'folder' => env('SPACES_FOLDER'),
+            'cdn_endpoint' => env('SPACES_CDN_ENDPOINT'),
+            'use_path_style_endpoint' => env('SPACES_USE_PATH_STYLE_ENDPOINT', false),
+            'throw' => false,
+        ]);
 
         // $app['config']->set('services.github', [
         //     'client_id' => 'github-client-id',
